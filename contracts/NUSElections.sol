e3482e465c67af8623233f2ccc03b2eb58edcde2
chenhuilong <huilong_1998@hotmail.com>
2022-03-14 10:30:50 +0800

    added tallyVote for NUSElections

--- contracts/NUSElections.sol
+++ contracts/NUSElections.sol
@@ -10,6 +10,7 @@ pragma solidity >=0.5.0;
  /** 
  * To-do:
  * 1. Use NUSToken once NUSToken contract is ready
+ * 2. Events 
  */
 
 contract NUSElections {
@@ -23,9 +24,12 @@ contract NUSElections {
 
     // NUSToken nusTokenInstance
     address public electionOwner;
-    mapping(address => Voter) public voters;
+    address[] voterList;
     uint256[] private votingOptions;
-    bool electionEnded;
+    mapping(address => Voter) public voters;
+    mapping(uint256 => uint256) public votingResults;
+    bool electionStatus;
+    uint256 totalVotes = 0;
 
     /** 
      * @dev Create a new election
@@ -34,14 +38,30 @@ contract NUSElections {
     constructor(uint256[] memory options) {
         electionOwner = msg.sender;
         votingOptions = options;
-        electionEnded = false;
+        electionStatus = false;
     }
 
+    // events 
+
+    event testVoting(uint256 votes);
+    // emit winner 
+    event voteMajority(uint256 voteID);
+    // emit draw 
+    // emit endVote 
+
     // modifiers 
-    modifier onGoingElection() {
+    modifier electionOngoing() {
         require(
-            !electionEnded, 
+            !electionStatus, 
             "Election has ended."
+        );
+        _;
+    }
+
+    modifier electionEnded() {
+        require(
+            electionStatus, 
+            "Election not ended yet."
         );
         _;
     }
@@ -57,23 +77,44 @@ contract NUSElections {
 
     // main functions 
 
-    function vote(uint256 votingChoice) onGoingElection validVotingChoice(votingChoice) public {
+    function vote(uint256 votingChoice) electionOngoing validVotingChoice(votingChoice) public {
         // uint256 memory curr_voter_NUST = nusTokenInstance.checkCredit();
         uint256 curr_voter_NUST = 1;
         Voter memory curr_voter = Voter(curr_voter_NUST, true, votingChoice);
-        
-        voters[msg.sender] = curr_voter;
-        
+        if (voters[msg.sender].voted == false) { // voter has not voted before 
+            voters[msg.sender] = curr_voter;
+            voterList.push(msg.sender);
+            totalVotes += curr_voter_NUST;
+        } else { // voter has voted before, changing vote 
+            voters[msg.sender] = curr_voter;
+        }
     }
 
-    function tallyVote() public {
+    function tallyVote() public electionOngoing {
         // count the votes 
-        electionEnded = true;
+        for (uint256 i=0; i<voterList.length; i++) {
+            Voter memory curr_voter = voters[voterList[i]];
+            votingResults[curr_voter.vote] += curr_voter.weight;
+        electionStatus = true;
+        }
     }
 
-    // getters 
+    function getVotingResult() public electionEnded returns(uint256[] memory) {
+        uint256[] memory results = new uint256[](votingOptions.length);
+        for (uint i = 0; i < votingOptions.length; i++) {
+            results[i] = votingResults[i];
+        }
+
+        return results;
+    }
+
+    // getters and helpers 
     function getVotingChoice() public view returns(uint256) {
         return voters[msg.sender].vote;
+    }
+
+    function getTotalVotes() public view returns(uint256) {
+        return totalVotes;
     }
 
 }
