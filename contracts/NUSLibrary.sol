c9215fe2d5d0dd09cbbc82a0f7a13ce16a2c0620
ivannedly <74553188+ivannedly@users.noreply.github.com>
2022-03-19 11:37:18 +0800

    remove listoflateborrowers

--- contracts/NUSLibrary.sol
+++ contracts/NUSLibrary.sol
@@ -6,23 +6,33 @@ import "./NUSToken.sol";
 */
 
 contract NUSLibrary {
-  address[] listOfLateBorrowers;
+  address admin;
   NUSToken nusToken;
   mapping(address => bool) isPaid;
 
-  function blacklist() public {
+  constructor(NUSToken nust) public {
+    nusToken = nust;
+  }
+
+  modifier isContractOwner() {
+        require(msg.sender == admin, "Not owner of contract");
+  }
+
+  function addBlacklist(address[] listOfLateBorrowers) public isContractOwner {
     uint256 length = listOfLateBorrowers.length;
     for (uint256 i = 0; i < length; i++) {
       address student = listOfLateBorrowers[i]; 
       require(nusToken.blacklistAddresses[student] != true); // make sure student is not an existing offender 
       nusToken.blacklistAddresses[student] = true; // add student to blacklist 
       isPaid[student] = false; // student marked as not paid
-    } 
+    }
   }
 
-  function payFine(address student) public {
+  function removeBlacklist(address student) public isContractOwner {
     require(isPaid[student] == false); // student must not have paid his/her fine yet
     nusToken.blacklistAddresses[student] = false; // remove from nustoken blacklist 
     isPaid[student] = true; // fine paid  
+
+    // fine tokens? or off chain pay for fine? 
   }
 }
