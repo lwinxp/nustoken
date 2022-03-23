fef83ed3f9b2e22120655926d1a6c83ebd4002a8
lwinxp <88076743+lwinxp@users.noreply.github.com>
2022-03-23 15:43:49 +0800

    Update migrations deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
 const ERC20 = artifacts.require("ERC20");
-const Pool = artifacts.require("Pool");
+const NUSToken = artifacts.require("NUSToken");
+const NUSModReg = artifacts.require("NUSModReg");
+const NUSElections = artifacts.require("NUSElections");
 
-module.exports = (deployer, network, accounts) => {
-    deployer.deploy(ERC20).then(function() {
-      return deployer.deploy(Pool);
-    });
-  };
+module.exports = async function(deployer) {
+  await deployer.deploy(ERC20);
+	await deployer.deploy(NUSToken);
+  await deployer.deploy(NUSModReg, NUSToken.address);
+	await deployer.deploy(NUSElections, [0,1], 2);
+};
