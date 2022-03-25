79779ffb3933209eea4d482ac11794c5ce36fc70
ivannedly <74553188+ivannedly@users.noreply.github.com>
2022-03-25 21:14:45 +0800

    included NUSLibrary

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,10 +2,12 @@ const ERC20 = artifacts.require("ERC20");
 const NUSToken = artifacts.require("NUSToken");
 const NUSModReg = artifacts.require("NUSModReg");
 const NUSElections = artifacts.require("NUSElections");
+const NUSLibrary = artifacts.require("NUSLibrary");
 
 module.exports = async function(deployer) {
   await deployer.deploy(ERC20);
 	await deployer.deploy(NUSToken);
   await deployer.deploy(NUSModReg, NUSToken.address);
-	await deployer.deploy(NUSElections, [0,1], 2, 1, NUSToken.address);
+  await deployer.deploy(NUSElections, [0,1], 2, 1, NUSToken.address);
+  await deployer.deploy(NUSLibrary, NUSToken.address);
 };
