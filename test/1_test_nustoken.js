fcc5372ed56fa655bb9b31a07b326501a8ae8198
Jones <joneslimwork@gmail.com>
2022-03-25 18:26:36 +0800

    Adding unit testing (incomplete)
    error in last section, fixing

--- test/1_test_nustoken.js
+++ test/1_test_nustoken.js
@@ -43,6 +43,7 @@ contract("NUSToken", function(accounts) {
         )
     })
 
+    // Sending tokens from supply pool to 1st account
     it("Send tokens from the Supply pool to accounts[1]", async () => {
         let account1Balance = await NUSTokenInstance.balanceOf(accounts[1])
         let gaveTokensFromSupply = await NUSTokenInstance.giveTokens(accounts[1], 1); // give 1 token
@@ -55,6 +56,7 @@ contract("NUSToken", function(accounts) {
         );
     })
 
+    // Taking tokens from 1st account and sending to supply pool
     it("Take token from accounts[1]", async () => {
         let account1Balance = await NUSTokenInstance.balanceOf(accounts[1])
         let contractBalance = await NUSTokenInstance.balanceOf(NUSTokenInstance.address)
@@ -78,6 +80,7 @@ contract("NUSToken", function(accounts) {
         );
     })
 
+    // Fining tokens from 1st account
     it("Fine accounts[1]!", async () => {
         let account1Balance = await NUSTokenInstance.balanceOf(accounts[1])
         let contractBalance = await NUSTokenInstance.balanceOf(NUSTokenInstance.address)
@@ -101,6 +104,7 @@ contract("NUSToken", function(accounts) {
         );
     })
 
+    // Semesterly token distribution of students
     it("Semester Token Distribution", async () => {
         let account1Balance = await NUSTokenInstance.balanceOf(accounts[1])
         let contractBalance = await NUSTokenInstance.balanceOf(NUSTokenInstance.address)
@@ -115,15 +119,137 @@ contract("NUSToken", function(accounts) {
         assert.notStrictEqual(
             account1BalanceAfter,
             account1Balance + 10000,
-            "Taking token from accounts[1] failed"
+            "Distributing tokens to accounts[1] failed"
+        );
+
+        assert.notStrictEqual(
+            BigInt(contractBalanceAfter),
+            BigInt(contractBalance - 10000),
+            "Taking tokens from contract failed"
+        );
+    })
+
+    // Taking tokens from graduating students and returning to pool
+    it("Retrieve all tokens from graduating/leaving student", async () => {
+        let account1Balance = await NUSTokenInstance.retrieveAllTokens(accounts[1])
+        let contractBalance = await NUSTokenInstance.balanceOf(NUSTokenInstance.address)
+
+        let account1BalanceAfter = await NUSTokenInstance.balanceOf(accounts[1])
+        let contractBalanceAfter = await NUSTokenInstance.balanceOf(NUSTokenInstance.address)
+
+
+        assert.strictEqual(
+            account1BalanceAfter.toNumber(),
+            0,
+            "Retrieving tokens from student failed"
         );
 
         assert.notStrictEqual(
             contractBalanceAfter,
-            contractBalance - 10000,
-            "Taking token from contract failed"
+            contractBalance + account1Balance,
+            "Contract did not receive the correct amount of tokens"
         );
     })
 
+    // Add/Remove address into whitelist addresses list and check if it is in
+    it("Add/Remove address into whitelisted addresses and check if in whitelist addresses", async () => {
+
+        // 0 refers to whitelist, true refers to adding to list
+        let addAddrIntoList = await NUSTokenInstance.modifyList(accounts[1], 0, true) 
+        let addressInList = await NUSTokenInstance.isAddressInWhitelistAddresses(accounts[1])
+        
+        assert.equal(
+            addressInList,
+            true,
+            "Address is not in the whitelist even though added"
+        );
+
+        // 0 refers to whitelist, false refers to removing from list
+        let removeAddrFromList = await NUSTokenInstance.modifyList(accounts[1], 0, false) 
+        let addressInList2 = await NUSTokenInstance.isAddressInWhitelistAddresses(accounts[1])
+
+        assert.equal(
+            addressInList2,
+            false,
+            "Address is still in the whitelist even though removed"
+        );
+    })
+
+    // Add/Remove address into blacklisted addresses list and check if it is in
+    it("Add/Remove address into blacklisted addresses and check if in blacklisted addresses", async () => {
+
+        // 1 refers to blacklist, true refers to adding to list
+        let addAddrIntoList = await NUSTokenInstance.modifyList(accounts[1], 1, true) 
+        let addressInList = await NUSTokenInstance.isAddressInBlacklistedAddresses(accounts[1])
+        
+        assert.equal(
+            addressInList,
+            true,
+            "Address is not in the blacklist even though added"
+        );
+
+        // 1 refers to blacklist, false refers to removing from list
+        let removeAddrFromList = await NUSTokenInstance.modifyList(accounts[1], 1, false) 
+        let addressInList2 = await NUSTokenInstance.isAddressInBlacklistedAddresses(accounts[1])
+
+        assert.equal(
+            addressInList2,
+            false,
+            "Address is still in the blacklist even though removed"
+        );
+    })
+
+    // Add/Remove address into list of addresses that can blacklist addresses and check if it is in
+    it("Add/Remove address into addresses that can blacklist addresses and check functionality", async () => {
+
+        // ADDING 
+
+        // 2 refers to list of addresses that can blacklist addresses, true refers to adding to list
+        let addAddrIntoList = await NUSTokenInstance.modifyList(accounts[1], 2, true) 
+        let addressInList = await NUSTokenInstance.isAddressInCanBlacklistAddresses(accounts[1])
+        
+        assert.equal(
+            addressInList,
+            true,
+            "1st Address cannot blacklist other addresses even though added"
+        );
+
+        // checking if this address can blacklist other addresses & checking if that address is in the blacklist
+        let addAddrIntoBlacklist = await NUSTokenInstance.modifyBlacklist(accounts[2], true, {from: accounts[1]}) //ERROR HERE
+        let addressIsInBlacklist = await NUSTokenInstance.isAddressInBlacklistedAddresses(accounts[2], {from: accounts[1]})
+
+        assert.equal(
+            addressIsInBlacklist,
+            true,
+            "2nd address is not in the blacklist even though added"
+        );
+
+        // REMOVAL
+
+        // 2 refers to list of addresses that can blacklist addresses, false refers to removing from list
+        let removeAddrFromList = await NUSTokenInstance.modifyList(accounts[1], 2, false) 
+        let addressInList2 = await NUSTokenInstance.isAddressInCanBlacklistAddresses(accounts[1])
+
+        assert.equal(
+            addressInList2,
+            false,
+            "Address can still blacklist other addresses even though removed"
+        );
+
+        // checking if this address can blacklist other addresses & checking if that address is in the blacklist
+        let removeAddrFromBlacklist = await NUSTokenInstance.modifyBlacklist(accounts[2], false, {from: accounts[1]})
+        let addressInBlacklist = await NUSTokenInstance.isAddressInBlacklistedAddresses(accounts[2])
+
+        assert.equal(
+            addressInBlacklist,
+            false,
+            "2nd address is in the blacklist even though removed"
+        );
+    })
+
+    // Add/Remove address into list of addresses that can fine addresses and check if it is in
+    it("Add/Remove address into addresses that can fine addresses and check functionality", async () => {
+        // similar to the above
+    })
 
 });
